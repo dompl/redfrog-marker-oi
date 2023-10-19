68c000a7eb541de238343e2f9eac55a151afa04b
Dom <info@redfrogstudio.co.uk>
2023-10-19 11:16:07 +0100

    added clicense file

--- redfrog-marker-oi.php
+++ redfrog-marker-oi.php
@@ -9,7 +9,7 @@
  * Author:            Dom Kapelewski
  * Author URI:        https://yourwebsite.com
  * Text Domain:       markerio-integration
- * License:           GPL v2 or later
+ * License:           GNU v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
